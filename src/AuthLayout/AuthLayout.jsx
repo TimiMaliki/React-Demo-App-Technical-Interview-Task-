bbc63e73b6aa074ae56c33d868b23e58968f6f65
TimiMaliki <malikitimi@gmail.com>
2025-08-28 19:51:03 +0100

    added : scroll restoration

--- src/AuthLayout/AuthLayout.jsx
+++ src/AuthLayout/AuthLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { ScrollRestoration } from "react-router-dom";
 import Header from "../pages/header";
 import Footer from "../pages/footer";
 
@@ -7,6 +8,7 @@ const AuthLayout = () => {
         <>
             <Header/>
             <Outlet/>
+            <ScrollRestoration/>
             <Footer/>
         </>
      );
