749cbc1fd27dc2cb14dca2cc52d734fa9f808ed2
TimiMaliki <malikitimi@gmail.com>
2025-08-28 17:27:20 +0100

    added : main page , API content , display content

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,79 +1,69 @@
 import { useState } from "react";
-
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    const [formData , setFormData] = useState({
-        email : "",
-        password : "",
-    })
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
+  const handleOnChange = (e) => {
+    setFormData(...formData, [e.target.name, e.target.value]);
+  };
 
-    const handleOnChange = (e) => {
-        setFormData(...formData , [e.target.name ,e.target.value])
-    }
-
-    const handleOnSubmit = (e) =>{
-        e.preventDefault()
-    }
-    return ( 
-       
-   <div className="min-h-screen flex items-center justify-center  bg-white px-4">
-
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center  bg-white px-4">
       <div className="w-full max-w-md bg-white border border-black shadow-xl rounded-2xl p-8">
-       
         <div className="text-center mb-8">
-          <h1 className="text-2xl font-bold  text-dark">
-            Welcome Back 
-          </h1>
-          <p className="text-dark text-sm mt-2">
-            Please login to your account
-          </p>
+          <h1 className="text-2xl font-bold  text-dark">Welcome Back</h1>
+          <p className="text-dark text-sm mt-2">Please login to your account</p>
         </div>
-    
+
         <form onSubmit={handleOnSubmit} className="space-y-6">
-            <div>
-              <label className="block text-gray-700 font-medium mb-1">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleOnChange}
-                className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 outline-none"
-                placeholder="john@example.com"
-                required
-              />
-            </div>
+          <div>
+            <label className="block text-gray-700 font-medium mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleOnChange}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 outline-none"
+              placeholder="john@example.com"
+              required
+            />
+          </div>
 
-            <div>
-              <label className="block text-gray-700 font-medium mb-1">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleOnChange}
-                className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 outline-none"
-                placeholder="••••••••"
-                required
-              />
-            </div>
-
+          <div>
+            <label className="block text-gray-700 font-medium mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleOnChange}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 outline-none"
+              placeholder="••••••••"
+              required
+            />
+          </div>
+          <Link to={"/"}>
             <button
               type="submit"
               className="w-full py-3 bg-gradient-to-r from-red-600 to-orange-500 text-white font-bold rounded-lg hover:opacity-90 transition"
             >
               Login
             </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-            
-          </form>
-        </div>
-
-         </div>
-     );
-}
- 
 export default Login;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+
 const Main = () => {
+    const [data , setData] = useState([])
+    useEffect(() =>{
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) =>res.json())
+        .then((data) => setData(data))
+        .catch((err) => console.log(err.message))
+
+    },[data])
     return ( 
-        <h1 className="text-3xl font-bold underline bg-red-400">
-        Hello world!
-      </h1>
+        <section className="min-h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-pink-100 py-10 px-4 lg:px-24">
+              <div className="cards-container mt-4 w-full h-full md:grid lg:grid grid md:grid-cols-2 lg:grid-cols-3  gap-4">
+
+{data.map((user)=> (
+    <div
+    className="card w-full h-full border border-red-300 rounded shadow-lg cursor-pointer hover:border-white hover:bg-black hover:text-white transition-all  duration-200 "
+    key={user.id}
+  >
+      <div className="w-full h-full mt-4 text-center">
+        <div className="content text-center">
+            <h4 className="text-sm font-semibold mb-4"> <span className="text-xl font-semibold"> User Id : </span> {user.id}</h4>
+          <h3 className="text-sm font-semibold mb-6">
+          <span className="text-xl font-semibold">Title : </span>{user.title}
+          </h3>
+          <p className="text-lg p-2">
+          <span className="text-xl font-semibold">Body : </span> {user.body}
+          </p>
+        </div>
+      </div>
+    </div>
+))}
+              </div>
+
+            </section>
      );
 }
  
