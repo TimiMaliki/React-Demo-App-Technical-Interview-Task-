4834757073f6c4e3ce7a77f1904c0d5ff2b063f8
TimiMaliki <malikitimi@gmail.com>
2025-08-28 19:11:57 +0100

    added : footer content

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
     return ( 
-        <div>
-            footer
+        <div className="footer text-center">
+     <div className="mt-8 border-t  pt-4 text-center text-sm">
+        &copy; {new Date().getFullYear()}  Timi Maliki.
+      </div>
         </div>
      );
 }
